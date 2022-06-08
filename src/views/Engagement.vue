f0a65f4bb17119d159e867c2375d6f35b9b3bfba
ayushkumar105 <ayush.kumar@tcu.edu>
2022-06-08 16:47:54 -0500

    Reordered accordion and added the remaining content in engagement

--- src/views/Engagement.vue
+++ src/views/Engagement.vue
@@ -3,75 +3,71 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-        Research 
+        Competitions and National Conferences
       </button>
     </h2>
     <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
         <div class="accordion-body">
-          Please, enter content here. 
+          The goal of the Competitions and National Conferences initiative of Neeley Complete is to present students with various opportunities to explore and engage in areas related to their prospective field and beyond. Through these experiences, students will have the ability to evolve professionally by networking, expanding their knowledge, presenting their ideas, and gaining new perspectives. 
         </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-        Intercultural Education
+        Employer-Based Work
       </button>
     </h2>
     <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
         <div class="accordion-body">
-            The purpose of the Intercultural Education initiative of Neeley Complete is to introduce students to cross-cultural competence and immerse them in global competencies to create impactful interactions with the world around them.      
+          The purpose of the employer-based work initiative of Neeley Complete is to present students with the real-world application of their studies. Employer-based work may be done in any related field to the student's studies. Practical application helps determine interests, increase future job satisfaction, and demonstrates the importance of company culture fit. Through employer-based work, students have the opportunity to network and create lifelong professional connections.        </div>
         </div>
-    </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-        Leadership
+        Intercultural Education 
       </button>
     </h2>
     <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
         <div class="accordion-body">
-            Please, enter content here.    
-        </div>
+          The purpose of the Intercultural Education initiative of Neeley Complete is to introduce students to cross-cultural competence and immerse them in global competencies to create impactful interactions with the world around them.        </div>
     </div>
   </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="panelsStayOpen-headingFour">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-          Service
+          Leadership
         </button>
       </h2>
       <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
         <div class="accordion-body">
-            Please, enter content here.        
-        </div>
+          The purpose of the Leadership initiative of Neeley Complete is to encourage students to use their unique strengths and skill sets to share passions and develop leadership skills. Leadership may take the form of formal and informal leadership positions where students make a sizable impact on others and are growing in the process.        </div>
       </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="panelsStayOpen-headingFive">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-            Competitions and National Conferences 
+            Research 
             </button>
         </h2>
         <div id="panelsStayOpen-collapseFive" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFive">
             <div class="accordion-body">
-                The goal of the Competitions and National Conferences initiative of Neeley Complete is to present students with various opportunities to explore and engage in areas beyond their field or interest. Through these experiences, students will have the ability to evolve professionally by networking, expanding their knowledge, presenting their ideas, and gaining new perspectives.        
-            </div>
+              The purpose of the Research initiative of Neeley Complete is to allow students to explore the academic side of their studies further. Through research, the student works to become an expert in an area which promotes critical thinking and clarifies passions for future endeavors. Through assisting a faculty member with research, the student has the opportunity to learn alongside some of the most distinguished researchers in the field.            </div>
         </div>
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="panelsStayOpen-headingSix">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseSix" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-          Employer-Based Work
+          Service
         </button>
       </h2>
       <div id="panelsStayOpen-collapseSix" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingSix">
         <div class="accordion-body">
-            Please, enter content here.
+          The purpose of the Service initiative of Neeley Complete is to encourage students to experience the unique environment and culture of serving others while connecting service opportunities with students' interests. Service allows students to gain different perspectives on areas of potential impact while inspiring students to become future ethical business leaders.      
         </div>
-      </div>
     </div>
+  </div>
 </div>
 </template>
 
